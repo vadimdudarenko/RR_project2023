cdace5abeb50cf1d891f37893f48a51636b58a93
ivangrakhovski444422 <128736660+ivangrakhovski444422@users.noreply.github.com>
2023-06-16 20:31:28 +0200

    Update project.R
    comment

--- project.R
+++ project.R
@@ -12,7 +12,8 @@ library(ggplot2)
 matches_1930_2022 <- read.csv("matches_1930_2022.csv")
 world_cup <- read.csv("world_cup.csv")
 
-####Data preparation###
+####Data preparation### 
+#Deliting unnecessary columns
 
 matches <- matches_1930_2022[, -c(9, 10, 11, 12, 14,15,18,19,20,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45)]
 
