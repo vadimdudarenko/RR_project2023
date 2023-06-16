17a0b104e36eb03afd0bbb30cd75ae0ce4f18366
Ismayil Ismayilov <100377925+ismayil-ismayilov@users.noreply.github.com>
2023-06-16 23:21:45 +0200

    Fix the chart #1

--- project.R
+++ project.R
@@ -57,6 +57,7 @@ championships
 # Chart #1
 # It makes a bar chart showing how many World Cup Championships each country has won. The chart is neat and clean with blue bars.
 # It tries to add information about the number of championships won by home and away teams in a dataset named matches.
+# Create a countplot
 ggplot(world_cup, aes(x = Champion)) +
   geom_bar(fill = "steelblue") +
   labs(x = "Country", y = "Number of World Cup Championships") +
@@ -64,15 +65,15 @@ ggplot(world_cup, aes(x = Champion)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
+# Add new variables to matches data frame
 matches$Home_Team_Champions <- 0
 matches$Away_Team_Champions <- 0
 
+# Update new variables with championships data
+matches$Home_Team_Champions <- ifelse(matches$home_team %in% names(championships), 
+                                       championships[matches$home_team], 
+                                       0)
+matches$Away_Team_Champions <- ifelse(matches$away_team %in% names(championships), 
+                                       championships[matches$away_team], 
+                                       0)
 
-if (!is.null(championships[matches$home_team])) {
-  matches$Home_Team_Champions <- championships[matches$home_team]
-}
-
-if (!is.null(championships[matches$away_team])) {
-  matches$Away_Team_Champions <- championships[matches$away_team]
-}
