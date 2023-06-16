f535ddf7ed35d26876a7a4b15a365aa36909e2e3
ivangrakhovski444422 <128736660+ivangrakhovski444422@users.noreply.github.com>
2023-06-16 20:29:41 +0200

    Update project.R
    actions with columns

--- project.R
+++ project.R
@@ -13,3 +13,17 @@ matches_1930_2022 <- read.csv("matches_1930_2022.csv")
 world_cup <- read.csv("world_cup.csv")
 
 ####Data preparation###
+
+matches <- matches_1930_2022[, -c(9, 10, 11, 12, 14,15,18,19,20,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45)]
+
+
+
+replace_teams <- function(matches) {
+  matches$home_team[matches$home_team %in% c("German DR", "Germany FR")] <- "Germany"
+  matches$home_team[matches$home_team == "Soviet Union"] <- "Russia"
+  
+  matches$away_team[matches$away_team %in% c("German DR", "Germany FR")] <- "Germany"
+  matches$away_team[matches$away_team == "Soviet Union"] <- "Russia"
+  
+  return(matches)
+}
